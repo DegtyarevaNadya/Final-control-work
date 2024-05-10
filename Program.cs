354cde3e8868eb437da9dc995e5e49472a3a7cf2
Надежда Дегтярева <adya19941@rambler.ru>
2024-05-10 20:00:30 +0300

    Add files via upload
    Основа - запрос массива и вывод массива (неизмененного)

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MainHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int change = 0;
+            Console.WriteLine("Если вы хотите использовать ваш массив, введите 1. Использовать массив по умолчанию - 2: ");
+            change = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Вы ввели {change}");
+
+            string[] example = new string[6] { "123", "4563", "example", "array", "what", "43" };
+            string[] array = example;
+
+            switch (change)
+            {
+                case 1: 
+                    Console.WriteLine("Вы решили использовать ваш массив");
+                    array = InputArrayStrings(6);
+                    break;
+                case 2:
+                    Console.WriteLine("Вы решили использовать массив по умолчанию");
+                    break;
+                default:
+                    Console.WriteLine("Не понял вас");
+                    break;
+            }
+            PrintArrayStrings(array);
+            Console.ReadKey();
+        }
+        static string[] InputArrayStrings(int arraySize)
+        {
+            string[] strings = new string[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"Введите {i + 1} строку массива:");
+                strings[i] = Console.ReadLine();
+            }
+            return strings;
+        }
+
+        static void PrintArrayStrings(string[] array)
+        {
+            for(int i = 0; i < array.Length;i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+
+        }
+
+    }
+
+    
+}
